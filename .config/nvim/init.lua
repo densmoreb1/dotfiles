0126c181adb3f26f7f567b9340421cf1bd513a5e
Brandon Densmore <densmoreb1@icloud.com>
2025-02-19 08:11:22 -0500

    config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -97,6 +97,9 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
