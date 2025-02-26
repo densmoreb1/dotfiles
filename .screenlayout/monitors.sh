9d40419e89af4c02207c2f1f996fde618c6169e5
Brandon Densmore <densmoreb1@icloud.com>
2025-02-26 07:58:31 -0500

    config

--- .screenlayout/monitors.sh
+++ .screenlayout/monitors.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-xrandr --auto
-xrandr --output eDP-1 --auto \
-       --output DP-1-1.1 --mode 1920x1080 --rate 60 --right-of eDP-1 \
-       --output DP-1-1.2 --mode 1920x1080 --rate 60 --right-of DP-1-1.1
-
+xrandr --output eDP-1 --primary --output DP-1-0.1 --auto --right-of eDP-1 --output DP-1-0.2 --auto --right-of DP-1-0.1
