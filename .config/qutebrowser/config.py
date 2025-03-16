493e421b6bc9d332d97766a1364358465c202534
Brandon Densmore <densmoreb1@icloud.com>
2025-03-16 18:20:05 -0400

    config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,17 +1,21 @@
 config.load_autoconfig(False)
 
-c.content.cookies.accept = 'no-3rdparty'
+c.content.cookies.accept = "no-3rdparty"
 c.content.cookies.store = True
 
-c.downloads.location.directory = '~/downloads'
+c.downloads.location.directory = "~/downloads"
 
-c.zoom.default = '125%'
-c.fonts.default_size = '11pt'
+c.zoom.default = "125%"
+c.fonts.default_size = "11pt"
 
 c.colors.webpage.darkmode.enabled = True
-config.bind('po', 'config-cycle colors.webpage.darkmode.enabled')
+config.bind("po", "config-cycle colors.webpage.darkmode.enabled")
 
-config.bind('gh', 'open -- http://192.168.0.70:8081/')
-config.bind('st', 'open -- qute://settings/')
-config.bind('sb', 'open -- qute://bindings/')
-config.bind('clh', 'history-clear')
+config.bind("gh", "open -- http://192.168.0.70:8081/")
+config.bind("st", "open -- qute://settings/")
+config.bind("sb", "open -- qute://bindings/")
+config.bind("clh", "history-clear")
+
+c.fonts.web.family.sans_serif = "JetbrainsMono Nerd Font"
+c.fonts.web.family.standard = "JetbrainsMono Nerd Font"
+c.fonts.web.family.fixed = "JetbrainsMono Nerd Font"
