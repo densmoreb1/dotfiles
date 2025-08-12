ced1b067a603351b5e57bbe54fe3d19e2294f1a1
Brandon Densmore <densmoreb1@icloud.com>
2025-08-12 15:08:13 -0400

    config

--- everything_arch.sh
+++ everything_arch.sh
@@ -3,13 +3,18 @@
 # Ran archinstall choosing hyprland
 
 # Basic System
-sudo pacman -S alacritty qutebrowser fish startship waybar stow brightnessctl 
+sudo pacman -S alacritty qutebrowser fish startship waybar stow 
+sudo pacman -S brightnessctl tlp fprintd hyprpaper
 
 # Utils
-sudo pacman -S task neovim docker
+sudo pacman -S task neovim docker pass wl-clipboard
 
 # Fonts
 sudo pacman -S ttf-jetbrains-mono-nerd otf-font-awesome
+
+# Start tlp
+sudo systemctl enable tlp
+sudo systemctl start tlp
 
 # Start docker
 sudo systemctl enable docker
