0231446ff86dff3742780b600b1f92e4dc3de844
Brandon Densmore <densmoreb1@icloud.com>
2025-08-12 20:59:06 -0400

    config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -11,13 +11,12 @@ c.fonts.default_size = "11pt"
 c.colors.webpage.darkmode.enabled = True
 config.bind("po", "config-cycle colors.webpage.darkmode.enabled")
 
-config.bind("gh", "open -- http://192.168.0.70:8081/")
 config.bind("st", "open -- qute://settings/")
 config.bind("sb", "open -- qute://bindings/")
 config.bind("clh", "history-clear")
 
-c.fonts.web.family.sans_serif = "JetbrainsMono Nerd Font"
-c.fonts.web.family.standard = "JetbrainsMono Nerd Font"
-c.fonts.web.family.fixed = "JetbrainsMono Nerd Font"
+# c.fonts.web.family.sans_serif = "JetbrainsMono Nerd Font"
+# c.fonts.web.family.standard = "JetbrainsMono Nerd Font"
+# c.fonts.web.family.fixed = "JetbrainsMono Nerd Font"
 
 c.content.tls.certificate_errors = "ask-block-thirdparty"
