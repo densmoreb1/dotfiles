ac6d41304d0dd0ddd81b69de187b9b633e0aff17
Brandon Densmore <hens.tippet.1o@icloud.com>
2025-08-19 20:13:21 -0400

    config fix

--- systems/modules/git.nix
+++ systems/modules/git.nix
@@ -2,7 +2,7 @@
   programs.git = {
     enable = true;
     userName = "Brandon Densmore";
-    userEmail = "densmoreb1@icloud.com";
+    userEmail = "hens.tippet.1o@icloud.com";
     extraConfig = {
       init.defaultBranch = "main";
       core.pager = "less -F -X";
