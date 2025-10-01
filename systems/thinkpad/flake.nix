b0b6eeab4e46dcb23d6fa75ff431a0f18d006ede
Brandon Densmore <hens.tippet.1o@icloud.com>
2025-10-01 16:34:21 -0400

    update

--- systems/thinkpad/flake.nix
+++ systems/thinkpad/flake.nix
@@ -2,7 +2,7 @@
   description = "My laptop config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     nixvim.url = "github:nix-community/nixvim";
     sops-nix.url = "github:Mic92/sops-nix";
