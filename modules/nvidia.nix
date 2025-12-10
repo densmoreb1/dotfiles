0e50eba312d33d0d3ce4b03a57937767f75ff9c4
Brandon Densmore <hens.tippet.1o@icloud.com>
2025-12-09 21:23:02 -0500

    removed nvidia

---
+++ modules/nvidia.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  # Dedicated Graphics
+  hardware.graphics = {
+    enable = true;
+  };
+  services.xserver.videoDrivers = ["modesetting" "nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  specialisation = {
+    on-the-go.configuration = {
+      system.nixos.tags = ["on-the-go"];
+      hardware.nvidia = {
+        prime.offload.enable = lib.mkForce true;
+        prime.offload.enableOffloadCmd = lib.mkForce true;
+        prime.sync.enable = lib.mkForce false;
+      };
+    };
+  };
+}
--- systems/thinkpad/configuration.nix
+++ systems/thinkpad/configuration.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -53,37 +48,6 @@
   services.xserver.excludePackages = with pkgs; [
     xterm
   ];
-
-  # Dedicated Graphics
-  hardware.graphics = {
-    enable = true;
-  };
-  services.xserver.videoDrivers = ["modesetting" "nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    prime = {
-      sync.enable = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
-  };
-
-  specialisation = {
-    on-the-go.configuration = {
-      system.nixos.tags = ["on-the-go"];
-      hardware.nvidia = {
-        prime.offload.enable = lib.mkForce true;
-        prime.offload.enableOffloadCmd = lib.mkForce true;
-        prime.sync.enable = lib.mkForce false;
-      };
-    };
-  };
 
   # Steam
   programs.steam = {
