8c3562e3b9b0b3be35857cb80e96a23f430e5db7
Brandon Densmore <hens.tippet.1o@icloud.com>
2026-01-03 18:16:26 -0500

    bluetooth

--- systems/rose/configuration.nix
+++ systems/rose/configuration.nix
@@ -10,6 +10,21 @@
 
   networking.hostName = "rose";
 
+  # Bluetooth
+  services.blueman.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true; # Show battery charge of Bluetooth devices
+      };
+    };
+  };
+
+  # Xbox controller
+  hardware.xone.enable = true;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -32,6 +47,7 @@
     bibata-cursors
     firefox
     gnupg # pass
+    mangohud
     hyprpaper
     localsend
     pass # passwords
