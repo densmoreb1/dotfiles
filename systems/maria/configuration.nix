8ef43d5923761abd5b5f5b6e14148e4030aa217f
Brandon Densmore <hens.tippet.1o@icloud.com>
2026-01-26 18:26:40 -0500

    powersaving

--- systems/maria/configuration.nix
+++ systems/maria/configuration.nix
@@ -5,5 +5,14 @@
 
   virtualisation.docker.enable = true;
 
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+      PLATFORM_PROFILE_ON_AC = "balanced";
+      WIFI_PWR_ON_AC = "on";
+    };
+  };
+
   system.stateVersion = "24.11";
 }
