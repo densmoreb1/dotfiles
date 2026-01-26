eb09129159265bca951bb743795fb8d8cc8eecbf
Brandon Densmore <hens.tippet.1o@icloud.com>
2026-01-25 20:14:44 -0500

    timeout

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -142,7 +142,7 @@
             yaml = ["prettier"];
           };
           format_on_save = {
-            timeout_ms = 500;
+            timeout_ms = 5000;
             lsp_format = "fallback";
           };
         };
