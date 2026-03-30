3c96c577bcd1a789ed4497460e98bef5db3f15fa
Brandon Densmore <hens.tippet.1o@icloud.com>
2026-03-30 14:02:46 -0400

    remove nvidia

--- systems/thinkpad/configuration.nix
+++ systems/thinkpad/configuration.nix
@@ -5,7 +5,6 @@
     ../../modules/desktop/remove-cookies.nix
     ../../modules/system/bluetooth.nix
     ../../modules/system/default.nix
-    ../../modules/system/nvidia.nix
     ../../modules/system/style.nix
   ];
 
@@ -37,7 +36,7 @@
   services.tlp = {
     enable = true;
     settings = {
-      START_CHARGE_THRESH_BAT0 = 79;
+      START_CHARGE_THRESH_BAT0 = 70;
       STOP_CHARGE_THRESH_BAT0 = 100;
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_BOOST_ON_AC = 1;
