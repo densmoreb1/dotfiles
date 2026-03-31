2ef1c51ab75bba6f433788d5be48129f666bba8d
Brandon Densmore <hens.tippet.1o@icloud.com>
2026-03-31 18:02:13 -0400

    reorg

--- systems/pipboy/default.nix
+++ systems/pipboy/default.nix
@@ -1,6 +1,43 @@
 {
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
   imports = [
-    ./configuration.nix
-    ./hardware-configuration.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ../../modules/system/default.nix
   ];
+
+  networking.hostName = "pipboy";
+
+  virtualisation.docker.enable = true;
+
+  system.stateVersion = "24.11";
+
+  # Hardware
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-intel"];
+  boot.extraModulePackages = [];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/151e61d4-b548-4c52-8453-63820438dba1";
+    fsType = "ext4";
+  };
+
+  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/4e0737a6-0160-4f6b-9b61-4ff7f1c09cf8";
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/606C-9DDE";
+    fsType = "vfat";
+    options = ["fmask=0022" "dmask=0022"];
+  };
+
+  swapDevices = [];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- systems/rose/default.nix
+++ systems/rose/default.nix
@@ -1,6 +1,70 @@
 {
+  config,
+  jovian,
+  lib,
+  modulesPath,
+  username,
+  ...
+}: {
   imports = [
-    ./configuration.nix
-    ./hardware-configuration.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
+    jovian.nixosModules.default
+    ../../modules/desktop/hyprland.nix
+    ../../modules/desktop/mail.nix
+    ../../modules/desktop/remove-cookies.nix
+    ../../modules/system/bluetooth.nix
+    ../../modules/system/default.nix
+    ../../modules/system/style.nix
   ];
+
+  networking.hostName = "rose";
+
+  # Xbox controller
+  hardware.xone.enable = true;
+
+  # Audio
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Steam
+  jovian = {
+    steam.enable = true;
+    steam.user = username;
+    hardware.has.amd.gpu = true;
+  };
+
+  # Enable Docker
+  virtualisation.docker.enable = true;
+
+  system.stateVersion = "25.11";
+
+  # Hardware
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/effbb375-18d4-4948-8813-8e72bf66fb8e";
+    fsType = "ext4";
+  };
+
+  boot.initrd.luks.devices."luks-dba8fa5f-bee4-4336-b85a-ee95fb08b755".device = "/dev/disk/by-uuid/dba8fa5f-bee4-4336-b85a-ee95fb08b755";
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/C8FB-6B76";
+    fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
+  };
+
+  swapDevices = [];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
