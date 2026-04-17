906f7392d7d4482a60ace48a2dc5d2c679b5b2a9
Brandon Densmore <hens.tippet.1o@icloud.com>
2026-04-17 16:28:07 -0400

    added drive

--- systems/maria/default.nix
+++ systems/maria/default.nix
@@ -32,9 +32,9 @@
   system.stateVersion = "24.11";
 
   # Hardware
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "ehci_pci" "sd_mod"];
-  boot.initrd.kernelModules = ["amdgpu"];
-  boot.kernelModules = [];
+  boot.initrd.availableKernelModules = ["vmd" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
   fileSystems."/boot" = {
@@ -43,36 +43,39 @@
     options = ["fmask=0022" "dmask=0022"];
   };
 
+  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/57cfeea5-2c36-4ce5-9ae6-a9c90bb07fa8";
+  boot.initrd.luks.devices."cryptmediaroot".device = "/dev/disk/by-uuid/292751bb-6145-4492-9fb5-e1b3eecc751a";
+  boot.initrd.luks.devices."cryptmedia1".device = "/dev/disk/by-uuid/d8122567-0284-4989-8402-4d36a358f75d";
+  boot.initrd.luks.devices."cryptmedia2".device = "/dev/disk/by-uuid/429508c1-d8a1-4385-b77b-6db7781add70";
+  boot.initrd.luks.devices."cryptmedia3".device = "/dev/disk/by-uuid/548def7c-1f1a-47d5-8548-d9530a305f16";
+
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/47deaee9-9beb-4a16-b070-45f3cc76783f";
     fsType = "ext4";
   };
-
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/57cfeea5-2c36-4ce5-9ae6-a9c90bb07fa8";
 
   fileSystems."/mnt/mediaroot" = {
     device = "/dev/disk/by-uuid/f6000c3d-e79f-42a4-86be-4306ab440c40";
     fsType = "ext4";
   };
 
-  boot.initrd.luks.devices."cryptmediaroot".device = "/dev/disk/by-uuid/292751bb-6145-4492-9fb5-e1b3eecc751a";
-
   fileSystems."/mnt/media1" = {
     device = "/dev/disk/by-uuid/ee6a2939-61b3-43a1-9cd7-ced460c91f2b";
     fsType = "ext4";
   };
-
-  boot.initrd.luks.devices."cryptmedia1".device = "/dev/disk/by-uuid/d8122567-0284-4989-8402-4d36a358f75d";
 
   fileSystems."/mnt/media2" = {
     device = "/dev/disk/by-uuid/b4a1e533-812f-4c54-8f53-590811536442";
     fsType = "ext4";
   };
 
-  boot.initrd.luks.devices."cryptmedia2".device = "/dev/disk/by-uuid/429508c1-d8a1-4385-b77b-6db7781add70";
+  fileSystems."/mnt/media3" = {
+    device = "/dev/disk/by-uuid/ac1168df-4e9c-4cf7-8d83-dcee9f73910f";
+    fsType = "ext4";
+  };
 
   fileSystems."/media" = {
-    device = "/mnt/media1:/mnt/media2:/mnt/mediaroot";
+    device = "/mnt/media1:/mnt/media2:/mnt/media3:/mnt/mediaroot";
     fsType = "fuse.mergerfs";
     options = [
       "defaults"
@@ -88,5 +91,5 @@
   swapDevices = [];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
