1ee471923b05d1da9b13bb6e8fc15edc5f2e9e48
Brandon Densmore <hens.tippet.1o@icloud.com>
2026-07-15 07:53:49 -0400

    raid

--- systems/maria/hardware-configuration.nix
+++ systems/maria/hardware-configuration.nix
@@ -18,12 +18,13 @@
 
   boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/57cfeea5-2c36-4ce5-9ae6-a9c90bb07fa8";
   boot.initrd.luks.devices."cryptmedia1".device = "/dev/disk/by-uuid/d8122567-0284-4989-8402-4d36a358f75d";
-  boot.initrd.luks.devices."cryptmedia2".device = "/dev/disk/by-uuid/429508c1-d8a1-4385-b77b-6db7781add70";
-  boot.initrd.luks.devices."cryptmedia3".device = "/dev/disk/by-uuid/548def7c-1f1a-47d5-8548-d9530a305f16";
 
   boot.swraid = {
     enable = true;
-    mdadmConf = "ARRAY /dev/md0 metadata=1.2 UUID=f6d83731:c69c6cbc:668333c7:7aac033e";
+    mdadmConf = ''
+      ARRAY /dev/md0 metadata=1.2 UUID=f6d83731:c69c6cbc:668333c7:7aac033e
+      MAILADDR root
+    '';
   };
   boot.initrd.luks.devices."cryptmediaraid".device = "/dev/disk/by-uuid/c557383b-ccb2-4ff0-8f93-55a51872d924";
 
@@ -42,18 +43,8 @@
     fsType = "ext4";
   };
 
-  fileSystems."/mnt/media2" = {
-    device = "/dev/disk/by-uuid/b4a1e533-812f-4c54-8f53-590811536442";
-    fsType = "ext4";
-  };
-
-  fileSystems."/mnt/media3" = {
-    device = "/dev/disk/by-uuid/ac1168df-4e9c-4cf7-8d83-dcee9f73910f";
-    fsType = "ext4";
-  };
-
   fileSystems."/media" = {
-    device = "/mnt/media1:/mnt/mediaraid1:/mnt/media3";
+    device = "/mnt/media1:/mnt/mediaraid1";
     fsType = "fuse.mergerfs";
     options = [
       "defaults"
